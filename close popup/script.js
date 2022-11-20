7619a5646bdefb6aee7e59dc284245fee4414d59
Vilpheraa <115183813+Vilpheraa@users.noreply.github.com>
2022-11-21 00:40:42 +0100

    modal popup comiit

---
+++ close popup/script.js
@@ -0,0 +1,12 @@
+document.querySelector("#open").addEventListener("click", function () {
+    document.querySelector(".openpop").classList.add("active");
+    
+});
+document.querySelector("#close").addEventListener("click", function () {
+    document.querySelector(".openpop").classList.remove("active");
+    
+});
+
+// function openPopup() {
+//     popup.style.display = "block";
+// }
